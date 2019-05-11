210aa27e4e5ef80169eec5aa4ed6e546e03502a4
Hai-Yong Jiang <haiyong.jiang@nlpr.ia.ac.cn>
2019-05-11 23:52:46 +0800

    revise parameters, and add outputs, and tune for rotation and scaling

--- ghough_transform/build/bin/main.py
+++ ghough_transform/build/bin/main.py
@@ -11,7 +11,7 @@ import shlex
 from subprocess import Popen, PIPE
 import cv2
 
-def ghough_transform(fpath_main, fpath_template, th):
+def ghough_transform(fpath_main, fpath_template):
     """
     params:
         @fpath_main: file path to the main image
@@ -20,28 +20,50 @@ def ghough_transform(fpath_main, fpath_template, th):
     returns:
         @rects: [B, 4, 2], a list of bounding box corners
     """
-    print("Processing " + fpath_main)
-    img = cv2.imread(fpath_template)
-    area = np.sum(img > 128)
-    print("img.size: ", img.shape, ", area: ", area)
+    print("*****************************************************")
+    print("Processing ", fpath_main + ", " + fpath_template)
+    img_ref = cv2.imread(fpath_main)
+    img_template = cv2.imread(fpath_template)
+    area = np.sum(img_template > 200)
+    print("img.size: ", img_template.shape, ", area: ", area)
     cmd = "./example_cpp_generalized_hough -i=" + fpath_main + " -t="\
-        + fpath_template + " --votesThreshold=%d"%(area//5) +\
+        + fpath_template + " --votesThreshold=%d"%(int(area*0.8)) +\
+        " --scaleThresh=%d --angleThresh=%d"%(area*20, area*100) +\
+        " --posThresh=%d"%(int(area*0.8)) +\
         " --minScale=0.2 --maxScale=2.0 --scaleStep=0.1" +\
-        " --minAngle=0.0 --maxAngle=180 --angleStep=3"
+        " --minAngle=0 --maxAngle=360 --angleStep=3" +\
+        " --full --maxBufSize=500000"
     #  os.system(cmd)
     process = Popen(shlex.split(cmd), stdout=PIPE)
-    process.communicate()
-    exit_code = process.wait()
+    (output, err) = process.communicate()
+    print(output, err)
+    process.wait()
 
     with open(fpath_main + ".txt", "r") as f:
         lines = f.readlines()
         rects = [[float(v) for v in l.strip().split(" ")] for l in lines]
     rects = np.array(rects, dtype=np.int32).reshape(-1, 4, 2)
+
+    #  for ii in range(rects.shape[0]):
+    #      pts = rects[ii].reshape(-1, 2)
+    #      cv2.polylines(img_ref, [pts], True, (0, 255, 255))
+    #  cv2.imshow("Image", img_ref)
+    #  cv2.waitKey(0)
+
     print("#detections: ", rects.shape[0])
+    fname = os.path.basename(fpath_template)
+    print("Results are saved to " + fpath_main + "_" + fname + ".txt")
+    save_corners(fpath_main + ".txt", rects)
     return rects
 
+def save_corners(fpath, corners):
+    with open(fpath, "w") as f:
+        for bb in corners:
+            bb = np.array(bb).reshape(-1)
+            f.write(" ".join([str(v) for v in bb]) + "\n")
+
 def test_ghough():
-    ghough_transform("images/Input1.png", "images/Input1Ref.png", th=200)
+    ghough_transform("images/Input1.png", "images/Input3Ref.png")
 
 if __name__ == "__main__":
     test_ghough()
--- template_match/main.py
+++ template_match/main.py
@@ -9,6 +9,7 @@
 # Python program to illustrate
 # multiscaling in template matching
 import cv2
+import os
 import numpy as np
 
 def nms(locs, scores, th):
@@ -41,7 +42,7 @@ def nms(locs, scores, th):
     return locs[:,idxs_select], scores[idxs_select], idxs_select
 
 
-def match_template(fpath_mainimage, fpath_template, th=0.3):
+def match_template(fpath_mainimage, fpath_template, th=0.35, minscale=0.5, maxscale=2.0, scalestep=0.1):
     """
     params:
         @fpath_mainimage: file path of image to be matched
@@ -50,7 +51,8 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
     returns:
         @corners: [N, 4, 2], oriented bb for matched instances
     """
-    print("Processing ", fpath_mainimage)
+    print("*****************************************************")
+    print("Processing ", fpath_mainimage + ", " + fpath_template)
 	# load data
     img_rgb = cv2.imread(fpath_mainimage)
     if len(img_rgb.shape) == 3:
@@ -63,28 +65,33 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
     img_gray = cv2.Canny(img_gray, 150, 200)
     template = cv2.Canny(template, 150, 200)
 
+    #  cv2.imshow("template", template)
+    #  cv2.imshow("Rotated", img_gray)
+    #  cv2.waitKey(0)
+
     w, h = template.shape[::-1] ## order: (h,w)
     print("shape vs template: ", img_rgb.shape, (h,w))
 
     ## find objects in different rotations and scales
     th_nms = np.minimum(w,h)*0.7
     cands = []
-    for rot in np.linspace(0, 180, 60):
+    for rot in np.linspace(0, 360, 120):
         center = (w/2, h/2)
         M = cv2.getRotationMatrix2D(center, rot, 1)
         corners = np.array([[0, 0, 1], [w, 0, 1], [w, h, 1], [0, h, 1]]).transpose(1, 0) #[3, 4]
         corners_rot = np.matmul(M, corners).transpose(1, 0)
-        corners_rot = np.maximum(0.0, corners_rot)
+        #  corners_rot = np.maximum(0.0, corners_rot)
         w_r, h_r = np.max(corners_rot, axis=0).astype(np.int32)
 
         #  template_rot = cv2.warpAffine(template, M, (w,h), borderValue=(255, 255, 255))
         template_rot = cv2.warpAffine(template, M, (w_r,h_r), borderValue=(0, 0, 0))
-        for scale in np.linspace(0.2, 2.0, 25)[::-1]:
+        for scale in np.linspace(minscale, maxscale, int((maxscale-minscale)/scalestep))[::-1]:
             resized = cv2.resize(
                 img_gray,
                 dsize = (int(img_gray.shape[1] * scale), int(img_gray.shape[0] * scale)),
                 interpolation=cv2.INTER_LANCZOS4
             )
+
             #  cv2.imshow("template", template_rot)
             #  cv2.imshow("Rotated", resized)
             #  cv2.waitKey(0)
@@ -93,6 +100,8 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
                 break
             s_r = img_gray.shape[0] / resized.shape[0] #[h, w]
             res = cv2.matchTemplate(resized, template_rot, cv2.TM_CCOEFF_NORMED)
+            #  res = cv2.matchTemplate(resized, template_rot, cv2.TM_CCORR_NORMED)
+
             max_loc = np.array(np.where(res>=th)[::-1])
             max_val = np.array(res[res>=th])
             max_loc, max_val, _ = nms(max_loc, max_val, th_nms)
@@ -101,10 +110,15 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
 
             hash_val = (max_loc[0]*s_r).astype(np.int32)*1024 + (max_loc[1]*s_r).astype(np.int32)
             max_loc = (np.array(max_loc) * s_r).transpose(1,0).astype(np.int32)
+            #  idxs = nms(max_loc.transpose(1,0), max_val, th_nms)[-1]
+            idxs = np.argsort(max_val)[::-1][:50]
             if max_val.shape[0] > 0:
                 #  print("detected", max_loc, max_val)
                 vals = [hash_val, max_val, max_loc, np.ones(hash_val.shape)*s_r, np.ones(hash_val.shape)*rot,
                         corners_rot.reshape(1, 4, 2)]
+                for ii in range(5):
+                    vals[ii] = vals[ii][idxs]
+
                 if len(cands) == 0:
                     cands = vals
                 else:
@@ -116,7 +130,7 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
         print("No shape is detected.")
         return
     else:
-        print("Detected: ", cands[2])
+        print("Detected: ", np.array(cands[2]).shape[0])
     ## filter multiple detections on the same location
     hash_val, max_val, max_loc, s_r, rot, corners = cands
     max_loc = max_loc.transpose(1,0)
@@ -126,32 +140,30 @@ def match_template(fpath_mainimage, fpath_template, th=0.3):
     ## object patches (I did not add rotations)
     max_loc, s_r, rot, corners = cands_filter
     max_loc = np.array(max_loc).astype(np.int32)
-    print("Filtered results: ", max_loc.transpose(1,0))
-
-
-    #  (startX, startY) = max_loc[0], max_loc[1]
-    #  (endX, endY) = max_loc[0] + (w * s_r).astype(np.int32), max_loc[1] + (h * s_r).astype(np.int32)
-    # draw a bounding box around the detected result and display the image
-    #  cv2.rectangle(img_rgb, (startX, startY), (endX, endY), (0, 0, 255), 5)
-    #  cv2.rectangle(img_rgb, max_loc[0], max_loc[0], (0, 0, 255), 5)
-    #  for ii in range(len(startX)):
-    #      cv2.rectangle(img_rgb, (startX[ii], startY[ii]),
-    #                    (endX[ii], endY[ii]), (0, 0, 255), 5)
+    print("Filtered results: ", max_loc.shape[1], np.max(max_val))
 
     corners = corners + max_loc.transpose(1,0).reshape(-1, 1, 2)
     corners = corners.astype(np.int32)
-    for ii in range(len(corners)):
-        pts = corners[ii].reshape(-1, 1, 2)
-        cv2.polylines(img_rgb, [pts], True, (0, 255, 255))
-
+#      for ii in range(len(corners)):
+    #      pts = corners[ii].reshape(-1, 1, 2)
+    #      cv2.polylines(img_rgb, [pts], True, (0, 255, 255))
     #  cv2.imshow("Image", img_rgb)
     #  cv2.waitKey(0)
+
+    fname = os.path.basename(fpath_template)
+    print("Results are saved to " + fpath_mainimage + "_" + fname + ".txt")
+    save_corners(fpath_mainimage + ".txt", corners)
     return corners
 
+def save_corners(fpath, corners):
+    with open(fpath, "w") as f:
+        for bb in corners:
+            bb = np.array(bb).reshape(-1)
+            f.write(" ".join([str(v) for v in bb]) + "\n")
 
 def test_tm():
     match_template("images/Input1.png", "images/Input2Ref.png")
 
 if __name__ == "__main__":
-    test_tm()
+    match_template("images/Input2.png", "images/Input2Ref.png")
 
